e8b8607ff369312fe9e6b61830df367b0220eb98
Ciliste <95741289+Ciliste@users.noreply.github.com>
2022-06-15 09:13:24 +0200

    Update FrameCreation.java

--- appli1/ihm/FrameCreation.java
+++ appli1/ihm/FrameCreation.java
@@ -6,12 +6,25 @@ import appli1.Controleur;
 import java.awt.BorderLayout;
 
 public class FrameCreation extends JFrame {
+    
+    private static final int RIEN    = 0;
+    private static final int NOUVEAU = 1;
+    private static final int OUVERT  = 2;
 
+    private static final int AUCUN   = 0;
+    private static final int TRAVAIL = 1;
+    
     private Controleur ctrl;
+
+    private int statutFichier;
+    private int statutTravail;
 
     public FrameCreation(Controleur ctrl) {
 
         this.ctrl = ctrl;
+
+        this.statutFichier = FrameCreation.RIEN;
+        this.statutTravail = FrameCreation.AUCUN;
 
         this.setTitle("Création d'un réseau");
         this.setSize(500, 500);
@@ -29,7 +42,18 @@ public class FrameCreation extends JFrame {
 
     public void nouveau() {
 
-        this.add(new JScrollPane(new PanelCreation(ctrl, this, null)), BorderLayout.CENTER);
+        if (this.statutTravail == FrameCreation.AUCUN) {
+
+            this.statutFichier = FrameCreation.NOUVEAU;
+            this.statutTravail = FrameCreation.TRAVAIL;
+
+            this.add(new JScrollPane(new PanelCreation(ctrl, this, null)), BorderLayout.CENTER);
+        }
+        else {
+
+            int result = JOptionPane.showConfirmDialog(this, "Press any button to close the dialog.");
+        }
+        
         this.pack();
     }
 
